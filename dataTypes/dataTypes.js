9b1cf8bc0ee6b1ce87d44f8ca07cc47477cf4103
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2023-05-11 14:06:29 +0200

    data types js

---
+++ dataTypes/dataTypes.js
@@ -0,0 +1,12 @@
+// DATA TYPES
+// Primitive and Non-primitive types/references
+//Primitive types
+// 1 . String
+// 2 . Number
+// 3 . Boolean
+// 4 . Null
+// 5 . Undefined
+// 6 . Symbol() ES6
+const user = "john";
+console.log(user);
+console.log(typeof user);
